195545eb678c030562ac66a84f31e8ba536a1099
MeryAmun <chebesiawah@gmail.com>
2023-02-01 16:26:10 +0100

    client take action

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { clients } from '../constants';
+import styles from '../style';
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () =>  (
+   <section className={`${styles.flexCenter} my-4`}>
+<div className={`${styles.flexCenter} flex-wrap w-full`}>
+  {
+    clients.map((client) => (
+<div className={`${styles.flexCenter} flex-1 flex-wrap w-full client-hover`} key={client.id}>
+  <img src={client.logo} alt="clients" className='sm:w-[192px] w-[100px] object-contain'/>
+</div>
+    ))
+  }
+</div>
+   </section>
   )
-}
 
 export default Clients
